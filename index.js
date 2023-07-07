e8dd040abab53730c1c92cf8481fbe5f3979ebbd
symatt <jamesxsy17@gmail.com>
2023-07-07 15:43:28 +0800

    added use dist

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ morgan.token('body', (request) => {
 	return JSON.stringify(request.body);
 });
 
+app.use(express.static('dist'));
 app.use(cors());
 app.use(express.json());
 app.use(morgan(':method :url :status - :body'));
